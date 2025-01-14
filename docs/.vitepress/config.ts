1babb2e229bf7ba0a4557c0496e1abf1ee025d65
leet <896600628@qq.com>
2025-01-14 14:12:38 +0800

    update 作品分类

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,49 +19,76 @@ export default defineConfig({
     nav: [
       { text: '主页', link: '/' },
       { text: '文档', link: '/introduction' },
+      { text: '项目', link: '/project/project' },
       { text: '作品', link: '/work' },
       { text: '制作者', link: '/team' },
     ],
 
-    sidebar: [
-      {
-        text: '简介',
-        collapsed: false,
-        link: '/introduction'
-      },
-      {
-        text: '功能实现',
-        collapsed: false,
-        items: [
-          { text: '静态资源请求处理器', link: '/function/ResourceHttpRequestHandler' },
-          { text: '服务器部署前后端+SSL证书', link: '/function/deploy+ssl' },
-          { text: 'Actions工作流(自动部署)', link: '/function/workflow' },
-          { text: '跨域处理', link: '/function/cors' },
-          { text: 'github3D指标统计', link: '/function/github3D' },
-          { text: '前端录音', link: '/function/recorder' },
-          { text: '前端视频流播放实现', link: '/function/video' },
-        ],
-      },
-      {
-        text: '踩坑记录',
-        collapsed: false,
-        items: [
-          { text: '2024踩坑日志', link: '/mistake/2024' },
-          { text: '2025踩坑日志', link: '/mistake/2025' },
-        ],
-      },
-      {
-        text: '知识',
-        collapsed: false,
-        items: [
-          { text: '小知识点', link: '/knowledge/smallpoint' },
-          { text: 'WebSocket', link: '/knowledge/ws' },
-          { text: 'SSE', link: '/knowledge/sse' },
-          { text: '定时任务', link: '/knowledge/cron' },
-          { text: 'playwright简单使用', link: '/knowledge/playwright' },
-          { text: '前端视频流基础', link: '/knowledge/video' },
-        ],
-      },
-    ],
+    sidebar: {
+      '/': { base: '/', items: sidebarDocx() },
+      '/project/': { base: '/project/', items: sidebarProject() }
+    },
   },
 });
+
+
+function sidebarDocx(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: '简介',
+      collapsed: false,
+      link: 'introduction'
+    },
+    {
+      text: '功能实现',
+      collapsed: false,
+      items: [
+        { text: '静态资源请求处理器', base: '/function/', link: 'ResourceHttpRequestHandler' },
+        { text: '服务器部署前后端+SSL证书', base: '/function/', link: 'deploy+ssl' },
+        { text: 'Actions工作流(自动部署)', base: '/function/', link: 'workflow' },
+        { text: '跨域处理', base: '/function/', link: 'cors' },
+        { text: 'github3D指标统计', base: '/function/', link: 'github3D' },
+        { text: '前端录音', base: '/function/', link: 'recorder' },
+        { text: '前端视频流播放实现', base: '/function/', link: 'video' },
+      ],
+    },
+    {
+      text: '踩坑记录',
+      collapsed: false,
+      items: [
+        { text: '2024踩坑日志', base: '/mistake/', link: '2024' },
+        { text: '2025踩坑日志', base: '/mistake/', link: '2025' },
+      ],
+    },
+    {
+      text: '知识',
+      collapsed: false,
+      items: [
+        { text: '小知识点', base: '/knowledge/', link: 'smallpoint' },
+        { text: 'WebSocket', base: '/knowledge/', link: 'ws' },
+        { text: 'SSE', base: '/knowledge/', link: 'sse' },
+        { text: '定时任务', base: '/knowledge/', link: 'cron' },
+        { text: 'playwright简单使用', base: '/knowledge/', link: 'playwright' },
+        { text: '前端视频流基础', base: '/knowledge/', link: 'video' },
+      ],
+    },
+  ]
+}
+
+function sidebarProject(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: '简介',
+      collapsed: false,
+      base: '/project/',
+      link: 'project'
+    },
+    {
+      text: '项目列表',
+      collapsed: false,
+      items: [
+        { text: '项目列表', base: '/project/', link: 'list' },
+      ],
+    },
+  ]
+}
