39a70c0c0f8eadaf8a9f41bdf3b9382795445479
leet <896600628@qq.com>
2025-04-07 14:54:30 +0800

    fix bug

---
+++ docs/index.vue
@@ -0,0 +1,21 @@
+<template>
+  
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  import('canvas-confetti').then((module) => {
+    module.default({
+      particleCount: 100,
+      spread: 170,
+      origin: { y: 0.6 },
+    });
+  })
+})
+</script>
+
+<style>
+
+</style>
