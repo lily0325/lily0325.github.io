e202a143e5046a6265f57e51598c614f4c5d3673
leet <896600628@qq.com>
2025-04-17 16:04:52 +0800

    Update index.vue

--- docs/index.vue
+++ docs/index.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <img class="count-img" src="https://count.getloli.com/@leet-code?name=leet-code&theme=booru-mjg&padding=7&offset=0&align=top&scale=0.3&pixelated=1&darkmode=auto" alt="leet-code" />
 </template>
 
 <script setup>
@@ -17,5 +17,9 @@ onMounted(() => {
 </script>
 
 <style>
-
+.count-img {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+}
 </style>
